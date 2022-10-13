77682cd96045e83d88ca830c661a9ca79be487ca
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-10-13 13:06:39 +0300

    added read file function

--- fs.py
+++ fs.py
@@ -5,3 +5,14 @@ ROOT = "www"
 
 def valid_path(path: str) -> bool:
     return os.path.isfile(ROOT + path)
+
+
+def read_file(path: str) -> bytes:
+    for root, dirs, files in os.walk(ROOT):
+        for name in files:
+            current_path = os.path.join(root, name)
+            print(current_path)
+            if path == current_path.lstrip("www"):
+                with open(current_path, "rb") as f:
+                    return f.read()
+    return b""
